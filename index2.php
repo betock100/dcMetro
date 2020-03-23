872bc71fedf08d6752166d63446cf3ee9cf1103d
betock100 <60243094+betock100@users.noreply.github.com>
2020-03-23 01:20:36 -0400

    Yes.
    My site.

---
+++ index2.php
@@ -0,0 +1,94 @@
+<?php include 'includes/header.php'?>
+    
+    
+    <main>
+
+          <article>
+            <section class="SectionStyle1">
+            <h> Renovations + Interiors + Exteriors</h>
+            </section>
+            <section class= "homeMid">
+                    <p>&nbsp;</p>
+            <h2>
+              Welcome to our site
+            </h2>
+              <p>Your home portrays who you are and reflects your unique taste and
+                  personality. So when you’re ready to make changes, whether it is
+                  making your kitchen more functional or updating your bathroom with a more contemporary look, Dc Metro Remodeling can help you create a
+                  space that best reflects the lifestyle you aspire to live.  
+              </p> 
+                        <p>&nbsp;</p>
+                    <h2>
+                      What we do
+                    </h2>
+                        <section class="int">
+                        <p>
+                            <span style="color: #db7f04; font-weight: bold; font-size: 28px">
+                                Interior: 
+                            </span> 
+                            Bathrooms, kitchens, basements, painting, staining, varnishing, masonry, finish carpentry, and much more.......
+                        </p>
+                        </section>
+                        <section class="ext">
+                        <p>
+                            <span style="color: #db7f04; font-weight: bold; font-size: 28px">
+                                Exterior: 
+                            </span> 
+                            Composite and pressure treated decking, porches, asphalt resurfacing, wood replacement, roofing, asphalt resurfacing, masonry, and much more.......
+                        </p>
+                        </section>
+                      <p>&nbsp;</p>
+                  
+                  </section>
+               
+                
+
+          
+          
+         
+
+                
+          
+
+            <!--
+          <a href="#" style="position: fixed; bottom: 500; color: black;"
+            >https://dcmetroremodeling.com/minuto30:46 video css playlist, video
+            responsive nav.
+          </a> 
+          -->
+    </main>
+
+    </article>
+      <section class="darker">
+          <div class="container2">
+            
+                <div class="d1">
+                <a style="color: white; text-decoration: none;"href="https://www.facebook.com/DcMetroRemodeling/"target="_blank">
+                  <div class="d1A">
+                  <img src="./images/fbSmall.PNG" alt="Like us!!" />
+                    You want to know what is going on at <br>DC Metro Remodeling LLC.  <br>
+                     Follow us in Facebook! 
+                  </div>
+                  </a>
+                  <a style="color: white; text-decoration: none;"href="tel:301-455-6258">
+                  <div class="d1B">
+                  <img src="./images/callUs2.PNG" alt="Call us!!" />
+                    Call us today for information at <br> 301-455-6258.
+                  </div>
+                  </a>
+                </div>
+          
+                  
+                  <div class="d2">
+                  <img src="./images/cCard.PNG" alt="Call us!!" />
+                      Make a payment now!. 
+                    </div>
+                
+                
+                  <!-- borrar class "d3" -->
+                  
+      
+          </div>
+      </section>
+    
+<?php include 'includes/footer.php'?>
